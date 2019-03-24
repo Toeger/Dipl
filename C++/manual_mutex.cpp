c634803214e8e03e94c5ee2881cfe8bdb004b0a5
Toeger <toeger@anony.mous>
2019-03-24 18:07:59 +0100

    Added manual mutex solution.

---
+++ C++/manual_mutex.cpp
@@ -0,0 +1,26 @@
+//compile with for example g++ -std=c++17 -pthread manual_mutex.cpp
+
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+int main() {
+	using Data = int;
+
+	Data data;
+	data += 1; //access without mutex protection
+	std::mutex mutex;
+	std::thread worker([&data, &mutex] {
+		mutex.lock();
+		data += 2; //access under mutex protection
+		mutex.unlock();
+		//data += 3; //no diagnostics
+	});
+	mutex.lock();
+	data += 4; //access under mutex protection
+	mutex.unlock();
+	//data += 5; //no diagnostics
+	worker.join();
+	data += 6;		   //access without mutex protection
+	std::cout << data; //access without mutex protection
+}
